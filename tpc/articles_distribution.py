7e0e811669430df4c9a03ca33c9ec966f8088109
Andrea Cortellari <ac.cortellari@gmail.com>
2024-04-15 00:57:15 +0200

    Update articles_distribution.py

--- tpc/articles_distribution.py
+++ tpc/articles_distribution.py
@@ -2,4 +2,4 @@ import pandas
 articles = pandas.read_csv(r'C:\\Users\\act1780\\Documents\\GitHub\\andreacortellari.github.io\\tpc\\Sources Database\\articles_view.csv', 
                            sep='\\t', usecols=['publication_title', 'issue_date', 'article_id'])
 pivoted_articles = articles.pivot_table(index=['publication_title'], columns=['issue_date'], aggfunc='count')
-pivoted_articles.to_csv('tpc\Data Jobs\Articles Distribution.csv')
+pivoted_articles.to_csv('Data Jobs\Articles Distribution.csv')
